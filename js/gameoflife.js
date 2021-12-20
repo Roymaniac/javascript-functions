e69b20532ea2ad041d6c2ada229c7ad1378c3d8f
Roymaniac <amolojoseph2020@gmail.com>
2021-12-20 01:41:07 +0100

    complete module

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -8,7 +8,7 @@ function same([x, y], [j, k]) {
 
 // The game state to search for `cell` is passed as the `this` value of the function.
 function contains(cell) {
-  return this.some((c) => same(c, cell));
+  return this.some(c => same(c, cell));
 }
 
 const printCell = (cell, state) => {
@@ -24,35 +24,34 @@ const corners = (state = []) => {
   }
 
   const xs = state.map(([x, _]) => x);
-  const xy = state.map(([_, y]) => y);
+  const ys = state.map(([_, y]) => y);
   return {
-    topRight: [Math.max(...xs), Math.max(...xy)],
-    bottomLeft: [Math.min(...xs), Math.min(...xy)]
+    topRight: [Math.max(...xs), Math.max(...ys)],
+    bottomLeft: [Math.min(...xs), Math.min(...ys)]
   };
 };
 
-const printCells = (state) => {
+const printCells = state => {
   const { bottomLeft, topRight } = corners(state);
   let accumulator = "";
   for (let y = topRight[1]; y >= bottomLeft[1]; y--) {
     let row = [];
     for (let x = bottomLeft[0]; x <= topRight[0]; x++) {
-      row.push(printCells([x, y], state));
+      row.push(printCell([x, y], state));
     }
     accumulator += row.join(" ") + "\n";
   }
-
   return accumulator;
 };
 
-const getNeighborsOf = ([x, y]) => {
-  [x-1, y+1], [x, y + 1], [x+1, y+1],
-  [x-1, y],               [x+1, y], 
-  [x-1, y-1], [x, y-1],   [x+1, y-1];
-};
+const getNeighborsOf = ([x, y]) => [
+    [x-1, y+1], [x, y+1], [x+1, y+1],
+    [x-1, y],             [x+1, y],
+    [x-1, y-1], [x, y-1], [x+1, y-1]
+];
 
 const getLivingNeighbors = (cell, state) => {
-  return getNeighborsOf(cell).filter((n) => contains.bind(state)(n));
+  return getNeighborsOf(cell).filter(n => contains.bind(state)(n));
 };
 
 const willBeAlive = (cell, state) => {
@@ -72,15 +71,15 @@ const calculateNext = (state) => {
       result = result.concat(willBeAlive([x, y], state) ? [[x, y]] : []);
     }
   }
-  return result
+  return result;
 };
 
 const iterate = (state, iterations) => {
-  const states = [state];
-  for (let i = 0; i < iterations; i++){
-    states.push(calculateNext(states[states.length - 1]));
-  }
-  return states;
+    const states = [state];
+    for(let i = 0; i < iterations; i++) {
+        states.push(calculateNext(states[states.length-1]));
+    }
+    return states;
 };
 
 const main = (pattern, iterations) => {
@@ -94,7 +93,7 @@ const startPatterns = {
     [2, 3],
     [3, 3],
     [3, 4],
-    [4, 4],
+    [4, 4]
   ],
   glider: [
     [-2, -2],
@@ -105,14 +104,14 @@ const startPatterns = {
     [2, 1],
     [3, 1],
     [3, 2],
-    [2, 3],
+    [2, 3]
   ],
   square: [
     [1, 1],
     [2, 1],
     [1, 2],
-    [2, 2],
-  ],
+    [2, 2]
+  ]
 };
 
 const [pattern, iterations] = process.argv.slice(2);
